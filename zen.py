95cc67c1433b240c14331b6b87245a26b1f8c85d
Andrey <andreeey.gg@gmail.com>
2022-01-29 22:10:58 +0100

    First commit

---
+++ zen.py
@@ -0,0 +1,44 @@
+import sys
+import this
+
+from random import choice
+
+from django.conf import settings
+from django.core.management import execute_from_command_line
+from django.http import HttpResponse
+from django.urls import path
+
+
+settings.configure(
+    ROOT_URLCONF=__name__,
+    DEBUG=True,
+    SECRET_KEY='secret'
+)
+
+text = ''.join(this.d.get(c, c) for c in this.s)
+title, _, *quotes = text.splitlines()
+
+
+template = """
+<!DOCTYPE html>
+<html>
+<head>
+ <title>{title}</title>
+</head>
+<body>
+ <h1>{quote}</h1>
+</body>
+</html>
+"""
+
+
+def hello(request):
+    return HttpResponse(template.format(title=title, quote=choice(quotes)))
+
+
+urlpatterns = [
+    path('', hello)
+]
+
+
+execute_from_command_line(sys.argv)
